24a8cbab92e7283d7ca4e57d4749d77dc6dd8f64
zlieberman <zlieberman99@gmail.com>
2021-05-15 15:05:55 -0400

    added further command line functionality

--- project1.cpp
+++ project1.cpp
@@ -7,11 +7,17 @@
 
 using namespace std;
 
-vector<int> generate_instance(size_t num_samples, int max_item_size) {
-    vector<int> vv;
-    for (size_t ii = 0; ii < num_samples; ++ii) {
-        vv.push_back(rand()%max_item_size);
-    }
+vector<int> generate_random_instance(size_t num_samples, int max_item_size) {
+    vector<int> vv(num_samples);
+    //https://stackoverflow.com/questions/21516575/fill-a-vector-with-random-numbers-c
+    auto gen = [&max_item_size](){return rand()%max_item_size;};
+    generate(vv.begin(), vv.end(), gen);
+    return vv;
+}
+
+vector<int> generate_constant_instance(size_t num_samples, int item_size) {
+    vector<int> vv(num_samples);
+    fill(vv.begin(), vv.end(), item_size);
     return vv;
 }
 
@@ -20,9 +26,11 @@ int get_idx(int value, int num_bins, int sample_idx) {
     return (value / (int)pow(num_bins,sample_idx)) % num_bins;
 }
 
-bool solve_instance(vector<int> sizes, int K, int B) {
+// solve an instance of the bin packing problem given the sizes
+// of the objects, the number of bins, and the capacity of each bin
+bool solve_instance(vector<int> sizes, int num_bins, int cap) {
     int num_samples = sizes.size();
-    double num_combinations = pow(K, num_samples);
+    double num_combinations = pow(num_bins, num_samples);
     // certificate to store the bin of each sample
     vector<int> bin_idxs;
     cout << "Number of possible combinations: " << num_combinations << endl;
@@ -30,11 +38,11 @@ bool solve_instance(vector<int> sizes, int K, int B) {
     for (int ii=0; ii<num_combinations; ++ii) {
         // always reinitialize the capacity of the bins
         // before trying a new combination
-        int bins[K];
-        fill_n(bins, K, B);
+        int bins[num_bins];
+        fill_n(bins, num_bins, cap);
         // put the balls in the appropriate bin for this combination
         for (int jj=0; jj<num_samples; ++jj) {
-            int bin_idx = get_idx(ii, K, jj);
+            int bin_idx = get_idx(ii, num_bins, jj);
             // add bin of each sample to the certificate
             bin_idxs.push_back(bin_idx);
             if (bins[bin_idx] >= sizes[jj]) {
@@ -52,9 +60,9 @@ bool solve_instance(vector<int> sizes, int K, int B) {
     return false;
 }
 
-bool verify_solution(const vector<int> sizes, const vector<int> certificate, const int K, const int B) {
-    int bins[K];
-    fill_n(bins, K, B);
+bool verify_solution(const vector<int> sizes, const vector<int> certificate, const int num_bins, const int cap) {
+    int bins[num_bins];
+    fill_n(bins, num_bins, cap);
     for (size_t ii=0; ii<sizes.size(); ++ii) {
         size_t idx = certificate[ii];
         if (bins[idx] >= sizes[ii]) {
@@ -68,7 +76,7 @@ bool verify_solution(const vector<int> sizes, const vector<int> certificate, con
 
 int main(int argc, char* argv[]) {
     // make sure the input has proper format
-    if (argc != 5) {
+    if (argc < 5) {
         cout << "Usage: ./prog num_samples max_item_size num_bins bin_capacity" << endl;
         return -1;
     }
@@ -84,14 +92,28 @@ int main(int argc, char* argv[]) {
     cout << "Number of bins: " << num_bins << endl;
     cout << "Capacity of each bin: " << bin_capacity << endl;
     // generate the problem instance using the parameters
-    vector<int> balls = generate_instance(num_samples, max_item_size);
-    /*
-    for (auto ball : balls) {
-        cout << ball;
+    vector<int> items;
+    // ask user for either constant or random sizes
+    while (1) {
+        char instance_type[1];
+        cout << "Type 'c' for constant or 'r' for random item sizes" << endl;
+        cin >> instance_type;
+        if (instance_type[0] == 'c') {
+            items = generate_constant_instance(num_samples, max_item_size);
+            break;
+        } else if (instance_type[0] == 'r') {
+            items = generate_random_instance(num_samples, max_item_size);
+            break;
+        } else {
+            cout << "Invalid input" << endl;
+        }
+    }
+    // print out the items
+    for (auto item : items) {
+        cout << item;
     }
     cout << endl;
-    */
-    bool solved = solve_instance(balls, num_bins, bin_capacity);
+    bool solved = solve_instance(items, num_bins, bin_capacity);
     cout << "Instance solved: ";
     cout << boolalpha << solved << endl;
     /*
