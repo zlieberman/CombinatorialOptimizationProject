b947dafd0f732ba1df278b68e731d61b17e3703c
PaulLevy1 <55198550+PaulLevy1@users.noreply.github.com>
2021-05-16 17:30:04 -0400

    Fixed bug
    fixed bug to adjust for the 't' case

--- save_text_instances.py
+++ save_text_instances.py
@@ -4,15 +4,15 @@ for i in range(1,9):
     count = 0
     string = ''
     for line in f:
-        if 'u' or 't' in line:
+        if 'u' in line or 't' in line:
             if count != 0:
-                f2 = open('/Users/paullevy/Desktop/Combinatorial Optimization/Project/Project 01/data/extracted/bp'+str(i)+'_' + str(count) + '.txt', 'w')
+                f2 = open('/Users/paullevy/Desktop/Combinatorial Optimization/Project/Project 01/data/extracted/'+str(i)+'/' + str(count) + '.txt', 'w')
                 f2.write(string)
                 f2.close()
             string = ''
             count += 1
         string += line
-    f2 = open('/Users/paullevy/Desktop/Combinatorial Optimization/Project/Project 01/data/extracted/bp'+str(i)+'_' + str(count) + '.txt', 'w')
+    f2 = open('/Users/paullevy/Desktop/Combinatorial Optimization/Project/Project 01/data/extracted/'+str(i)+'/' + str(count) + '.txt', 'w')
     f2.write(string)
     f2.close()
     f.close()
